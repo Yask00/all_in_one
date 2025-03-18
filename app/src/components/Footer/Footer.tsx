67bde280c71f7b0f2188f99536ce4c4a28f43858
Yasen Trayanov <jasstreb@abv.bg>
2025-03-18 20:42:27 +0200

    feat: add global  layout and main components for it

---
+++ app/src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from "react";
+import "./Footer.scss";
+
+const Footer = (): ReactElement => {
+  return <footer className="footer"></footer>;
+};
+
+export default Footer;
---
+++ app/src/components/Main/Main.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from "react";
+import "./Main.scss";
+
+const Main = (): ReactElement => {
+  return <main className="main"></main>;
+};
+
+export default Main;
