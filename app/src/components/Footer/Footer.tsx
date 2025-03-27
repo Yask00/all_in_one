941f5b6e7136d6963d7822414661799fa0ef7219
Yasen Trayanov <jasstreb@abv.bg>
2025-03-27 18:56:50 +0200

    feat: add date and clock in footer w Intl and Date

--- app/src/components/Footer/Footer.tsx
+++ app/src/components/Footer/Footer.tsx
@@ -1,8 +1,21 @@
 import { ReactElement } from "react";
 import "./Footer.scss";
+import Clock from "./Clock";
+
+const options: Intl.DateTimeFormatOptions = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
 
 const Footer = (): ReactElement => {
-  return <footer className="footer"></footer>;
+  const date = new Intl.DateTimeFormat("en-US", options).format(new Date());
+  return (
+    <footer className="footer">
+      {date} - <Clock />
+    </footer>
+  );
 };
 
 export default Footer;
